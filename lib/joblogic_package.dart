77a8bb995ff8a4a6ccd9efda18a17824344ea9c4
Brian Do <thuand@joblogic.com>
2022-05-23 16:12:54 +0700

    first commit

---
+++ lib/joblogic_package.dart
@@ -0,0 +1,17 @@
+library joblogic_package;
+
+import 'package:intl/intl.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  int addTwo(int value) => value + 2;
+  // var abv = DateFormat('yyyy-MM-dd');
+}
+
+String formatCurrentDateTime(DateTime now) {
+  final f = DateFormat('yyyy-MM-dd hh:mm');
+  return f.format(now);
+}
